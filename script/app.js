d0a7e17a3f674c47f9b48ee360f07f3f2d74db6a
Nima <nimah8310@gmail.com>
2026-08-01 16:26:55 +0330

    git commit -m "Delete app.js"

--- script/app.js
+++
@@ -1,8 +0,0 @@
-// Base Test
-const headerTheme  = document.querySelector('.header__util-btn--theme')
-const headerToggle = document.querySelector('.header__toggle-icon') 
-const headerMenu   = document.querySelector('.header__menu') 
-
-headerTheme.addEventListener('click' , ()  => document.body.classList.toggle('dark'))
-
-headerToggle.addEventListener('click' , () => headerMenu.classList.toggle('d-flex'))
