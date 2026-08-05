00971761df025c29c2dbf220e3d1a270ec1524a2
Nima <nimah8310@gmail.com>
2026-08-05 13:54:22 +0330

    feat(header): complete dynamic rendering for logo, menu, user profile, utility tools, mobile toggle, and auth buttons

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
 // ==========================================
 // 1. DOM Elements Selection (In HTML order)
 // ==========================================
-const headerLogo   = document.querySelector('.header__logo');
-const headerMenu   = document.querySelector('.header__menu');
-const menuToggleBtn = document.querySelector('#menuToggle');
-const headerTheme  = document.querySelector('.header__util-btn--theme');
+const headerPrimary = document.querySelector('.header__primary');
+const headerLogo    = document.querySelector('.header__logo');
+const headerMenu    = document.querySelector('.header__menu');
+const headerUser    = document.querySelector('.header__user');
+const headerUtility = document.querySelector('.header__utility');
+const headerAuth    = document.querySelector('.header__auth');
+
 
 // ==========================================
 // 2. Header Logo Configuration & Rendering
@@ -102,7 +105,42 @@ menuData.forEach((data) => {
 });
 
 // ==========================================
-// 5. Event Listeners & UI Interactions
+// 5. User Profile Configuration & Rendering
+// ==========================================
+const profileUser = {id : 1 , src : 'img/team4.png', className : 'header__user-img' , alt : 'user'}
+
+headerUser.insertAdjacentHTML('beforeend' , `<img class="${profileUser.className}" src="${profileUser.src}" alt="${profileUser.alt}">`)
+
+// ==========================================
+// 6. Utility Tools Data & Rendering (Language & Theme)
+// ==========================================
+
+const utilityToolsData = [
+    {id : 1 , className : 'header__util-btn header__util-btn--lang' , iconClass: 'ri-global-line header__util-icon'},
+    {id : 2 , className : 'header__util-btn header__util-btn--theme' , iconClass: 'ri-moon-clear-line header__util-icon'}
+];
+
+utilityToolsData.forEach(item => headerUtility.innerHTML += `<button class="${item.className}"><i class="${item.iconClass}"></i></button>`);
+
+const headerTheme = document.querySelector('.header__util-btn--theme');
+
+// ==========================================
+// 7. Toggle Button Configuration & Rendering
+// ==========================================
+const toggleBtnData = {className : 'header__toggle-btn' ,  id :'menuToggle' , ariaLabel : 'Toggle Menu' , iconClass: 'ri-menu-line header__toggle-icon'};
+
+headerPrimary.insertAdjacentHTML('beforeend' , `
+    <button
+        class="${toggleBtnData.className}"
+        id="${toggleBtnData.id}"
+        aria-label="${toggleBtnData.ariaLabel}">
+        <i class="${toggleBtnData.iconClass}"></i>
+    </button>
+    `)
+
+const menuToggleBtn = document.querySelector('#menuToggle');
+// ==========================================
+// 8. Event Listeners & UI Interactions
 // ==========================================
 
 // Toggle Mobile Navigation Menu & Icon State
@@ -120,3 +158,16 @@ menuToggleBtn.addEventListener('click', () => {
 headerTheme.addEventListener('click', () =>
   document.body.classList.toggle('dark')
 );
+
+// ==========================================
+// 9. Event Listeners Auth Buttons
+// ==========================================
+const headerAuthButtons = [
+    {id : 1 , className : 'btn btn--outline btn--login' , type : 'button', text : 'Log In' } ,
+    {id : 2 , className : 'btn btn--primary btn--signup' , type : 'button', text : 'Sign Up' }
+];
+
+headerAuthButtons.forEach(item => headerAuth.innerHTML += `<button class="${item.className}" type="${item.type}">${item.text}</button>`)
+
+
+
